d628cac3621c015d408e8dcf3e63ae7f9333a794
s.nakamura <shotenba@gmail.com>
2024-06-17 16:58:45 +0900

    apiの側を作成

--- main.go
+++ main.go
@@ -1,9 +1,31 @@
 package main
 
 import (
-	"fmt"
+    "github.com/gin-gonic/gin"
+    "net/http"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+    router := gin.Default()
+
+    // テンプレートファイルのロード
+    router.LoadHTMLGlob("templates/*")
+
+    // ルートハンドラ
+    router.GET("/", func(c *gin.Context) {
+		
+		imageURL, err := GenerateImage("クマの絵をお願いします", "1024x1024")
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		
+        c.HTML(http.StatusOK, "index.html", gin.H{
+            "Title":   "Hello, Gin!",
+            "Message": "Welcome to the Gin framework!",
+			"ImageURL": imageURL,
+        })
+    })
+	
+    router.Run(":8080")
 }
